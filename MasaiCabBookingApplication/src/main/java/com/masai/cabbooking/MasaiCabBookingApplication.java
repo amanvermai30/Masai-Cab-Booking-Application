01380a9e7934320907b9a62bb8cf9d6a7144c716
amanvermai30 <amancreative9598@gmail.com>
2023-03-06 12:57:19 +0530

    project started

---
+++ MasaiCabBookingApplication/src/main/java/com/masai/cabbooking/MasaiCabBookingApplication.java
@@ -0,0 +1,13 @@
+package com.masai.cabbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasaiCabBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasaiCabBookingApplication.class, args);
+	}
+
+}
